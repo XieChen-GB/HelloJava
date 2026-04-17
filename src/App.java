61b0689e1c08831f4480063fa98932fe87989f90
xiechen <xieche@gmail.com>
2026-04-17 22:13:15 +0900

    练习Java基本数据类型

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // 整数
+        int age =35;
+        // 小数
+        double height = 1.75;
+        // 布尔值
+        boolean isStudent = true;
+        // 字符
+        char firstChar = 'M';
+        // 字符串
+        String name = "田中";
+        
+        System.out.println("姓名: " + name);
+        System.out.println("年龄: " + age);
+        System.out.println("身高: " + height);
+        System.out.println("是否为学生: " + isStudent);
     }
 }
