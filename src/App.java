e01d9822d2013ebc9c986cd7cf0b84e47b088495
xiechen <xieche@gmail.com>
2026-04-27 10:39:33 +0900

    第8课Part1：ArrayList练习——学生成绩管理

--- src/App.java
+++ src/App.java
@@ -23,26 +23,50 @@ public class App {
         
         // 创建存放 Student 对象的 ArrayList
         ArrayList<Student> students = new ArrayList<>();
+        
+        // 添加5名学生
+        students.add(new Student("学生甲", 78));
+        students.add(new Student("学生乙", 81));
+        students.add(new Student("学生丙", 93));
+        students.add(new Student("学生丁", 55));
+        students.add(new Student("学生戊", 60));
 
-        // 添加学生 
-        students.add(new Student("谢晨", 85 ));
-        students.add(new Student("田中", 92));
-        students.add(new Student("山田", 67));
 
-        // 遍历所有学生
-        System.out.println("---所有学生---");
-        for (Student s : students){
+     
+        Student best = students.getFirst(), wrost = students.getFirst();
+        double average = 0.0;
+        //输出所有学生成绩
+        System.out.println("--所有学生---");
+        for(Student s : students){
             s.printInfo();
+            if (s.getScore() > best.getScore()) {
+                best = s;
+            } 
+            if (s.getScore() < wrost.getScore()) {
+                wrost = s;
+            }
+
+            average = average + s.getScore();
         }
 
-        // 找出成绩最高的学生
-        Student best = students.getFirst();
-        for ( Student s : students ){
-            if (s.getScore() > best.getScore()){
-                best = s;
+        System.out.println();
+        System.out.println("--成绩最高---");
+        best.printInfo();
+
+        System.out.println();
+        System.out.println("--成绩最低---");
+        wrost.printInfo();
+
+        System.out.println();
+        System.out.println("--平均分---");
+        average = average/students.size();
+        System.out.println(String.format("%.1f 分", average));
+
+        System.out.println("--及格学生---");
+        for (Student s : students) {
+            if (s.getScore() >= 60) {
+                s.printInfo();
             }
         }
-        System.out.println("---成绩最高---");
-        best.printInfo();
     }
 }
