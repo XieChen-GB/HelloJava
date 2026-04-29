3153020ad857c4ad3461aeea1aa4ce631ff17c1e
xiechen <xieche@gmail.com>
2026-04-29 10:51:58 +0900

    feature-loop：添加九九乘法表功能

--- src/App.java
+++ src/App.java
@@ -1,53 +1,14 @@
-import java.util.HashMap;
-
-class Student {
-    private String name;
-    private int score;
-
-    // 构造method
-    public Student (String name,int score){
-        this.name = name;
-        this.score = score;
-    }
-
-    // method
-    public int getScore(){ return score; }
-
-    public void printInfo(){
-        System.out.println("姓名：" + name + " 成绩：" + score);
-    }
-} 
-
-public class App {
+public class App{
     public static void main(String[] args) {
-        // Key = 学号，Value = Student 对象
-        HashMap<String,Student>  map = new HashMap<>();
-
-        map.put("S001", new Student("谢晨", 85));
-        map.put("S002", new Student("田中", 92));
-        map.put("S003", new Student("山田", 67));
-
-        // 用学号查找学生
-        System.out.println("---查找S002---");
-        map.get("S002").printInfo();
-
-        // 遍历所有学生
-        System.out.println("---所有学生---");
-        for (String id : map.keySet()){
-            System.out.print("学号：" + id + " ");
-            map.get(id).printInfo();
+        
+        // 新功能：用 for 循环打印九九乘法表
+        System.out.println("---九九乘法---");
+        for (int i= 1; i<=9; i++){
+            for (int j = 1; j<=i; j++){
+                System.out.print(j + "×" + i + "=" + (i * j) + "\t");
+            }
+            System.out.println();
         }
 
-        // 判断学号 S004 是否存在
-        if (map.containsKey("S004")) {
-            map.get("S004").printInfo();
-        } else {
-            System.out.println("学号 S004 不存在！");
-        }
-
-        // 更新 S001 的成绩
-        map.put("S001", new Student("谢晨", 99));
-        System.out.println("--- 更新后 S001 ---");
-        map.get("S001").printInfo();
     }
 }
